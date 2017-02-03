4470ce929df61f0fd7921b9587fbc5a968e512d3
aalmero <aalmero@gmail.com>
2017-02-03 17:46:03 -0500

    added Docker greeting

--- JavaMeetsDocker.java
+++ JavaMeetsDocker.java
@@ -3,5 +3,6 @@ public class JavaMeetsDocker{
   public static void main(String[] args){
     System.out.println("Java meets Docker!");
     System.out.println("Hello Java!");
+    System.out.println("Hi Docker!");
   }
 }
